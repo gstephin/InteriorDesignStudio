aa55cdd5b052d15e46f22c0c9c677f7d407443c6
Stephin George <gstephin@users.noreply.github.com>
2025-04-07 17:23:39 +0000

    map updates

--- client/src/components/contact-section.tsx
+++ client/src/components/contact-section.tsx
@@ -246,7 +246,7 @@ export default function ContactSection() {
               
               <div className="w-full h-[300px] rounded-lg overflow-hidden">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d543.0517702664479!2d76.2265694647379!3d10.017105041274432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0813141504750b%3A0x46611b3b224ae68c!2sKtisis%20Interiors!5e0!3m2!1sen!2sae!4v1744046208153!5m2!1sen!2sae" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d543.0517702664479!2d76.2265694647379!3d10.017105041274432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0813141504750b%3A0x46611b3b224ae68c!2sKtisis%20Interiors!5e0!3m2!1sen!2sae!4v1744046208153!5m2!1sen!2sae"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
